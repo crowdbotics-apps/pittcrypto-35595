f232d10220a62a68b2dccc176bd2fbbc29f88e94
Crowdbotics <team@crowdbotics.com>
2022-06-04 13:48:56 +0000

    Update models

--- backend/pittcrypto_35595/wsgi.py
+++ backend/pittcrypto_35595/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pittcrypto_35595.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pittcrypto_35595.settings")
 
 application = get_wsgi_application()
